c666a45006b0b35febd2f05b67a71a516f37603d
Yjames1 <Yjames@atl.yearup.org>
2015-04-24 10:01:53 -0400

    First Class Prjoct

---
+++ Intro/src/Intro.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+
+
+public class Intro 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Hello! I'm Yoshua. Welcome to the Java Class");
+
+		
+		
+		/*Invalid character constant.
+	 * Syntax error, insert")".
+	 * Syntax error, insert ";".
+	 * Syntax errors on taken, misplaced constructs.
+	 * Syntax Error on token "the", ; expected.
+	 *  Syntax error on token "}", delete this token.
+	 */
+	
+	}
+
+}
